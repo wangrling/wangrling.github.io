7db8e1f239d22628bd1c10d42c19a9393755e6cf
gaosheng <gaosheng@spreadst.com>
2017-04-11 13:59:15 +0800

    anotheroneone

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -4,7 +4,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Mozilla is cool! ' + myName;
 }
 
 
